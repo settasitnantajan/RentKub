81fcfbf7470ad881bd3c6d21b91da97ece1c67be
settasitnantajan <settasit.nantajan@gmail.com>
2025-03-19 21:31:46 +0700

    Create Dropdown Login/Logout
    -Clerk Login/Logout
    -fix Dropdown item

--- client/src/components/navbar/DropdownListMenu.jsx
+++ client/src/components/navbar/DropdownListMenu.jsx
@@ -6,39 +6,64 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { AlignLeft } from 'lucide-react';
+import { AlignLeft } from "lucide-react";
 import { Button } from "../ui/button";
 import UserIcon from "./UserIcon";
 import { Links } from "@/utils/Links";
 import { Link } from "react-router";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const DropdownListMenu = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-
         <Button variant="outline">
-            <AlignLeft />
-            <UserIcon />
+          <AlignLeft />
+          <UserIcon />
         </Button>
-
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
 
-        {
-            Links.map((item, index) => {
-                return (
-                    <DropdownMenuItem key={index}>
-                        <Link to={item.href}>
-                        {item.label}
-                        </Link>
-                    </DropdownMenuItem>
-                )
-            }
-                )
-        }
+        {Links.map((item, index) => {
+          return (
+            <DropdownMenuItem key={index}>
+              <Link to={item.href}>{item.label}</Link>
+            </DropdownMenuItem>
+          );
+        })}
+
+        <DropdownMenuSeparator />
+        {/* Login Show */}
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode="modal">
+              <button>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <SignUpButton mode="modal">
+              <button>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+
+        {/* Logout Show */}
+        <SignedIn>
+          <DropdownMenuItem>
+            {/* <UserButton/> */}
+            <SignOutButton />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
